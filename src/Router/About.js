5cc9f2c768fb1f13fc06e27989a35691104fcfa3
sandeep <maheshwaramsandeep8@gmail.com>
2022-08-15 14:34:01 +0530

    myPROJECT

---
+++ src/Router/About.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            
+            <h1>Atul , Boby,Viswnath good Morning</h1>
+         );
+    }
+}
+ 
+export default About;
